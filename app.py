6d36f26f65bd370094286528079e1570e2904ba6
Xander Gallegos <aranxa.gallegos@iteso.mx>
2025-09-25 12:50:43 -0600

    Funcinal Simplex Solver

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from jinja2 import pass_environment
-from pyarrow import null
+import numpy as np
 import streamlit as st
 import pandas as pd
 
@@ -10,8 +9,7 @@ def main_page():
         st.title("Método Simplex")
         size = st.number_input("Número de variables:", min_value=2, step=1)
         res = st.number_input("Número de restricciones:", min_value=1, step=1)
-        with st.popover("Valor de M:", width="stretch"):
-            M = st.number_input("", min_value=0, value=1000, step=1, label_visibility="collapsed")
+        M = st.number_input("Valor de M:", min_value=0, value=1000, step=1000)
 
         st.write("#")
 
@@ -19,6 +17,7 @@ def main_page():
             st.session_state["size"] = size
             st.session_state["res"] = res
             st.session_state["M"] = M
+            st.session_state["obj"] = st.session_state["objetivo"]
 
             matrix = pd.DataFrame()
             for ec in range(res+1):
@@ -34,7 +33,7 @@ def main_page():
                     matrix.loc[ec, "ld"] = st.session_state[f"{ec}-ld"]
             
             st.session_state["df"] = matrix
-            st.switch_page(st.Page(show_df))
+            st.switch_page(st.Page(solve))
 
 
     #st.header("Introduce tu matriz")
@@ -48,10 +47,10 @@ def main_page():
         for i, col in enumerate(cols_fo[:-1]):
             with col:
                 st.text(f"x{i+1}")
-                st.number_input("", value=0, step=1, key=f"0-x{i+1}", label_visibility="collapsed")
+                st.number_input("", value=0, key=f"0-x{i+1}", label_visibility="collapsed")
         with cols_fo[-1]:
             st.text("Objetivo")
-            st.selectbox("", ["Minimizar", "Maximizar"], label_visibility="collapsed", key="obj")
+            st.selectbox("", ["Minimizar", "Maximizar"], label_visibility="collapsed", key="objetivo")
 
     # Restricciones 
     with st.container():
@@ -62,7 +61,7 @@ def main_page():
             with col:
                 st.text(f"x{i+1}")
                 for r in range(1, res+1):
-                    st.number_input("", value=0, step=1, key=f"{r}-x{i+1}", label_visibility="collapsed")
+                    st.number_input("", value=0, key=f"{r}-x{i+1}", label_visibility="collapsed")
             
         with cols_res[-2]:
             st.text("Signo")
@@ -72,29 +71,111 @@ def main_page():
         with cols_res[-1]:
             st.text("LD")
             for r in range(1, res+1):
-                st.number_input("", value=0, step=1, key=f"{r}-ld", label_visibility="collapsed")
+                st.number_input("", value=0, key=f"{r}-ld", label_visibility="collapsed")
             
-def show_df():
+def solve():
+    st.set_page_config("Solución Simplex", layout="wide")
+
+    with st.sidebar:
+        st.title("Método Simplex")
+        if st.button("Regresar", type="primary", width="stretch"):
+            st.switch_page(st.Page(main_page))
+
+    up = st.container()
+
+    matrix = st.session_state.df.copy()
+
+    # Añadir variables de holgura y artificiales
     for r in range(1, st.session_state.res+1):
-        signo = st.session_state.df.loc[r, "s"]
+        signo = matrix.loc[r, "s"]
         if signo == "<=":
-            st.session_state.df.loc[r, f"s{r}"] = 1
+            matrix.loc[r, f"s{r}"] = 1
         elif signo == "=":
-            st.session_state.df.loc[r, f"a{r}"] = 1
+            matrix.loc[r, f"a{r}"] = 1
         else:
-            st.session_state.df.loc[r, f"s{r}"] = -1
-            st.session_state.df.loc[r, f"a{r}"] = 1
+            matrix.loc[r, f"s{r}"] = -1
+            matrix.loc[r, f"a{r}"] = 1
 
-    for col in st.session_state.df.columns:
+    # Llenar con 0s & quitar signos
+    matrix.fillna(0, inplace=True)
+    matrix.drop(columns="s", inplace=True)
+    #matrix.astype(float)
+
+    # Reordenar columnas
+    cols = ["z"] + [c for c in matrix.columns if c not in ["z", "ld"]] + ["ld"]
+    matrix = matrix[cols]
+
+    # Multiplicar x -1 si es necesario
+    for r in range(st.session_state.res+1):
+        if r == 0:
+            if st.session_state.obj == "Maximizar":
+                matrix.loc[r] = matrix.loc[r] * -1
+        else:
+            if matrix.loc[r, "ld"] < 0:
+                matrix.loc[r] = matrix.loc[r] * -1
+
+    # Multiplicar variables artificiales x M
+    a_var = []
+    for col in matrix.columns:
         if col.startswith("a"):
-            st.session_state.df.loc[0, col] = st.session_state.df[col].max() * st.session_state["M"]
-
-    st.session_state.df.fillna(0, inplace=True)
-    st.dataframe(st.session_state.df)
+            matrix.loc[0, col] = matrix[col].max() * st.session_state["M"]
+            a_var.append(col[1:])
     
+
+    # New 0
+    for a in a_var:
+        matrix.loc[0] -=  matrix.loc[int(a)].multiply(st.session_state.M)
+
+    i = 0
+    show_iter = st.expander("Ver iteraciones")
+
+    # start gauss-jordan loop
+    while matrix.drop(columns=["z", "ld"]).loc[0].min(numeric_only=True) < 0:
+        col_i, col_m = show_iter.container().columns([0.2,0.8])
+        col_i.text(f"Iteración {i}")
+        col_m.dataframe(matrix[cols])
+
+        pvt_col = matrix.drop(columns=["z", "ld"]).idxmin(axis="columns")[0]
+        matrix.loc[1:, "cruce"] = matrix.loc[1:, "ld"].divide(matrix.loc[1:, pvt_col])
+        matrix.loc[matrix.cruce < 0, "cruce"] = np.nan
+        pvt_row = matrix.idxmin(numeric_only=True).cruce
+
+        # make pvt = 1
+        pvt = matrix.loc[pvt_row, pvt_col]
+
+        if  pvt != 0:
+            matrix.loc[pvt_row, cols] /= pvt
+        
+        prvs = matrix.copy()
+
+        for ec in range(st.session_state.res+1):
+            if ec != pvt_row:
+                for var in cols:
+                    matrix.loc[ec, var] =  prvs.loc[ec, var] - prvs.loc[ec, pvt_col] * prvs.loc[pvt_row, var]
+        
+        i += 1
+        
+
+    if st.session_state.obj == "Minimizar":
+        matrix.loc[0] *= -1
+    
+
+    col_i, col_m = show_iter.container().columns([0.2,0.8])
+    col_i.text(f"Iteración {i}")
+    col_m.dataframe(matrix[cols])
+
+    up.latex(f"Z = {matrix.loc[0,'ld']:.2f}")
+
+    for var in cols[1:-1]:
+        if matrix[var].sum() == 1 and matrix[var].max() == 1:
+            res_row = matrix[var].idxmax()
+            res = matrix.loc[res_row, "ld"]
+            up.latex(f"{var[0]}_{var[1:]} = {res:.2f}")
+
+
 pg = st.navigation([
     st.Page(main_page),
-    st.Page(show_df)
+    st.Page(solve)
 ], position="hidden")
 
 pg.run()
